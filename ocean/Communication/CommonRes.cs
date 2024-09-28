21f14d7435117084c3fe925a660b6750dccf28c8
finhaz <finhaz@foxmail.com>
2024-09-28 19:38:44 +0800

    添加项目文件。

---
+++ ocean/Communication/CommonRes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Data;
+
+namespace ocean
+{
+    public class CommonRes
+    {
+        public static SerialPort mySerialPort = new SerialPort();
+
+        public static DataTable dt1 = new DataTable();
+        public static DataTable dt2 = new DataTable();
+        public static DataTable dt3 = new DataTable();
+    }
+
+
+
+}
---
+++ ocean/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using ocean.UI;
+using SomeNameSpace;
+using MenuItem = ocean.ViewModels.MenuItem;
+
+
+namespace ocean
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: MetroWindow
+    {
+        private readonly Navigation.NavigationServiceEx navigationServiceEx;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.navigationServiceEx = new Navigation.NavigationServiceEx();
+            this.navigationServiceEx.Navigated += this.NavigationServiceEx_OnNavigated;
+            this.HamburgerMenuControl.Content = this.navigationServiceEx.Frame;
+
+            // Navigate to the home page.
+            this.Loaded += (sender, args) => this.navigationServiceEx.Navigate(new Uri("Views/MainPage.xaml", UriKind.RelativeOrAbsolute));
+
+
+            //CommonRes.dt1 = DB_Access.GetDBTable("PARAMETER_RUN");
+            //CommonRes.dt2 = DB_Access.GetDBTable("PARAMETER_SET");
+            //CommonRes.dt3 = DB_Access.GetDBTable("PARAMETER_FACTOR");
+
+            CommonRes.dt1 = DB_sqlite.GetDBTable("PARAMETER_RUN");
+            CommonRes.dt2 = DB_sqlite.GetDBTable("PARAMETER_SET");
+            CommonRes.dt3 = DB_sqlite.GetDBTable("PARAMETER_FACTOR");
+
+        }
+
+
+        private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
+        {
+            if (e.InvokedItem is MenuItem menuItem && menuItem.IsNavigation)
+            {
+                this.navigationServiceEx.Navigate(menuItem.NavigationDestination);
+            }
+        }
+
+        private void NavigationServiceEx_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            // select the menu item
+            this.HamburgerMenuControl.SelectedItem = this.HamburgerMenuControl
+                                                         .Items
+                                                         .OfType<MenuItem>()
+                                                         .FirstOrDefault(x => x.NavigationDestination == e.Uri);
+            this.HamburgerMenuControl.SelectedOptionsItem = this.HamburgerMenuControl
+                                                                .OptionsItems
+                                                                .OfType<MenuItem>()
+                                                                .FirstOrDefault(x => x.NavigationDestination == e.Uri);
+
+            // or when using the NavigationType on menu item
+            // this.HamburgerMenuControl.SelectedItem = this.HamburgerMenuControl
+            //                                              .Items
+            //                                              .OfType<MenuItem>()
+            //                                              .FirstOrDefault(x => x.NavigationType == e.Content?.GetType());
+            // this.HamburgerMenuControl.SelectedOptionsItem = this.HamburgerMenuControl
+            //                                                     .OptionsItems
+            //                                                     .OfType<MenuItem>()
+            //                                                     .FirstOrDefault(x => x.NavigationType == e.Content?.GetType());
+
+            // update back button
+            this.GoBackButton.Visibility = this.navigationServiceEx.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void GoBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.navigationServiceEx.GoBack();
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show("数据将保存！");
+
+            //DB_Access.UpdateDBTable(CommonRes.dt1, "PARAMETER_RUN");
+            //DB_Access.UpdateDBTable(CommonRes.dt2, "PARAMETER_SET");
+            //DB_Access.UpdateDBTable(CommonRes.dt3, "PARAMETER_FACTOR");
+
+
+            DB_sqlite.UpdateDBTable(CommonRes.dt1, "PARAMETER_RUN");
+            DB_sqlite.UpdateDBTable(CommonRes.dt2, "PARAMETER_SET");
+            DB_sqlite.UpdateDBTable(CommonRes.dt3, "PARAMETER_FACTOR");
+        }
+    }
+}
